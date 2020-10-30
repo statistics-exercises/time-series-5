7971c5571b0a6753bc28f231572dc283246e8218
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 18:26:06 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def exponential(lam) :
+  # Your function to generate an exponential random variable goes here
+  
+def firstevent(lam1,lam2) : 
+  # Your function to return if Alice and Bob meet at the bus stop goes here
+  
+
+
+print( firstevent(5,4), firstevent(5,4), firstevent(5,4), firstevent(5,4), firstevent(5,4) )
